a17f3490597d1bc294a355e546862ba6df11a43d
scott <44701197+scott-x@users.noreply.github.com>
2019-07-07 22:56:07 +0800

    Update test.go

--- example/test.go
+++ example/test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/scott-x/go-commander/cmd"
 	"fmt"
 )
-func main(){
+func Example(){
    cmd.AddQuestion("name","What's your name ? ","Please input correct name: ","[a-z]+")
    cmd.AddQuestion("age","What's your age ? ","Please input correct age: ","[0-9]{2}")
    a :=cmd.Exec()
